ad64caff864ea0879b8d8e1f4b0bde2bd194a339
Joseph Lumbley <joelumbley16@gmail.com>
2023-04-01 21:26:04 -0500

    Clean up

--- Pong/Form1.vb
+++ Pong/Form1.vb
@@ -1,7 +1,7 @@
 ﻿'Pong
 'In this app we remake the classic ping pong game.
 'This version is resizable, pausable and has a computer player.
-'Supports keyboard, mouse, Xbox or PlayStation controllers.
+'Supports keyboard, mouse, Xbox and PlayStation controllers.
 'Written in VB.NET this year 2023 actually works on Windows 10 64-Bit.
 'I'm making a video to explain the code on my YouTube channel.
 'https://www.youtube.com/@codewithjoe6074
