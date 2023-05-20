ed546351cce6413c89ab36d41d01e30a3789fdde
Joseph Lumbley <joelumbley16@gmail.com>
2023-05-20 18:02:43 -0500

    Documentation

--- Pong/Form1.vb
+++ Pong/Form1.vb
@@ -1,8 +1,8 @@
 ﻿'Pong
 'In this app we remake the classic ping pong game.
 'This version is resizable, pausable and has a computer player.
-'Supports keyboard, mouse, Xbox controllers.
-'Written in VB.NET this year 2023 actually works on Windows 10 64-Bit.
+'Supports keyboard, mouse and Xbox controllers including the vibration effect (rumble).
+'It was written in VB.NET in 2023 and is compatible with Windows 10 and 11.
 'I'm making a video to explain the code on my YouTube channel.
 'https://www.youtube.com/@codewithjoe6074
 '
