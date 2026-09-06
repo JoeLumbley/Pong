520709d7568b6bda67a445e0ee66758337662151
Joseph Lumbley <joelumbley16@gmail.com>
2026-09-06 10:11:24 -0500

    Reduce loop and pause sound volumes
    Decreased "loop" sound volume from 900 to 300 and "pause" sound volume from 500 to 75 for better audio balance.

--- Pong/Form1.vb
+++ Pong/Form1.vb
@@ -307,7 +307,7 @@ Public Class Form1
         CreateSoundFiles()
 
         AudioPlayer.AddSound("loop", Path.Combine(Application.StartupPath, "loop.mp3"))
-        AudioPlayer.SetVolume("loop", 900)
+        AudioPlayer.SetVolume("loop", 300)
 
         AudioPlayer.AddSound("point", Path.Combine(Application.StartupPath, "point.mp3"))
         AudioPlayer.SetVolume("point", 500)
@@ -329,7 +329,7 @@ Public Class Form1
         AudioPlayer.SetVolume("select", 500)
 
         AudioPlayer.AddSound("pause", Path.Combine(Application.StartupPath, "pause.mp3"))
-        AudioPlayer.SetVolume("pause", 500)
+        AudioPlayer.SetVolume("pause", 75)
 
         AudioPlayer.AddSound("fullscreen", Path.Combine(Application.StartupPath, "fullscreen.mp3"))
         AudioPlayer.SetVolume("fullscreen", 500)
